cd3daa40fbc7d118c9af6ca3da0b894f376ccd88
AndrejKh <silverwewel@gmail.com>
2021-09-02 01:47:17 +0600

    Login Screen Added

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Image.asset("assets/img/bck_r.png"),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  alignment: Alignment.topRight,
+                  width: double.infinity,
+                  height: 300,
+                  child: Stack(
+                    children: [
+                      Image.asset("assets/img/vector_1_r.png"),
+                      Image.asset("assets/img/vector_2_r.png"),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Column(
+              children: [
+                SizedBox(
+                  height: 300,
+                ),
+                Container(
+                    padding: EdgeInsets.all(12),
+                    height: 83,
+                    child: Image.asset("assets/img/logo_2.png")),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
