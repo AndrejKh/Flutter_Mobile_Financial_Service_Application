2e3c054479e4c5b4f8a3719c6974b3490279d722
AndrejKh <silverwewel@gmail.com>
2021-09-02 01:58:21 +0600

    Update

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -33,14 +33,46 @@ class _LoginScreenState extends State<LoginScreen> {
               ],
             ),
             Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SizedBox(
-                  height: 300,
+                  height: 250,
                 ),
                 Container(
                     padding: EdgeInsets.all(12),
                     height: 83,
                     child: Image.asset("assets/img/logo_2.png")),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(
+                    "Log In",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(12, 0, 0, 10),
+                  child: Text(
+                    "Into Your E-Wallet",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 5, 30, 5),
+                  child: TextField(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 5, 30, 5),
+                  child: TextField(),
+                ),
+                SizedBox(
+                  height: 150,
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  height: 70,
+                  child: Image.asset("assets/img/login_btn.png"),
+                )
               ],
             )
           ],
