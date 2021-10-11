46937b4096eebee858b8808b5c526cf33b1c1efe
AndrejKh <silverwewel@gmail.com>
2021-10-11 18:08:42 +0600

    transaction successful screen created

---
+++ lib/screens/transaction_successfull.dart
@@ -0,0 +1,16 @@
+import 'package:etaka/components/constant.dart';
+import 'package:etaka/components/reuseable_widgets.dart';
+
+class TransactionSuccessful extends StatefulWidget {
+  const TransactionSuccessful({Key? key}) : super(key: key);
+
+  @override
+  _TransactionSuccessfullState createState() => _TransactionSuccessfullState();
+}
+
+class _TransactionSuccessfullState extends State<TransactionSuccessful> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
