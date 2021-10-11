06a48639a081792bb840a63f476d257b37e081db
AndrejKh <silverwewel@gmail.com>
2021-10-11 23:39:26 +0600

    updated transaction failed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:etaka/screens/transaction_failed.dart';
 import 'package:etaka/screens/transaction_successfull.dart';
 import 'package:etaka/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: TransactionSuccessful(),
+      home: TransactionFailed(),
     );
   }
 }
