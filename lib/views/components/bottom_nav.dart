8e2312f463321beb0081d86ae8aac79af362b690
AndrejKh <silverwewel@gmail.com>
2021-10-15 03:57:02 +0600

    Bottom Nav Bar Added

---
+++ lib/views/components/bottom_nav.dart
@@ -0,0 +1,223 @@
+import 'package:etaka/views/screens/dashboard_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
+
+// ----------------------------------------- Provided Style ----------------------------------------- //
+
+class DashboardMain extends StatefulWidget {
+  DashboardMain({Key? key}) : super(key: key);
+
+  @override
+  _DashboardMainState createState() => _DashboardMainState();
+}
+
+class _DashboardMainState extends State<DashboardMain> {
+  late PersistentTabController _controller;
+  late bool _hideNavBar;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = PersistentTabController(initialIndex: 0);
+    _hideNavBar = false;
+  }
+
+  List<Widget> _buildScreens() {
+    return [Dashboard(), Dashboard(), Dashboard(), Dashboard()];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.home),
+        title: "Home",
+        activeColorPrimary: Colors.blue,
+        inactiveColorPrimary: Colors.grey,
+        inactiveColorSecondary: Colors.purple,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.access_time),
+        title: ("History"),
+        activeColorPrimary: Colors.teal,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.account_circle_rounded),
+        title: ("Account"),
+        activeColorPrimary: Colors.blueAccent,
+        activeColorSecondary: Colors.indigo,
+        inactiveColorPrimary: Colors.grey,
+        // routeAndNavigatorSettings: RouteAndNavigatorSettings(
+        //   initialRoute: '/',
+        //   routes: {
+        //     '/first': (context) => MainScreen2(),
+        //     '/second': (context) => MainScreen3(),
+        //   },
+        // ),
+        // onPressed: (context) {
+        //   pushDynamicScreen(context,
+        //       screen: SampleModalScreen(), withNavBar: true);
+        // },
+      ),
+      // PersistentBottomNavBarItem(
+      //   icon: Icon(Icons.message),
+      //   title: ("Messages"),
+      //   activeColorPrimary: Colors.deepOrange,
+      //   inactiveColorPrimary: Colors.grey,
+      //   // routeAndNavigatorSettings: RouteAndNavigatorSettings(
+      //   //   initialRoute: '/',
+      //   //   routes: {
+      //   //     '/first': (context) => MainScreen2(),
+      //   //     '/second': (context) => MainScreen3(),
+      //   //   },
+      //   //),
+      // ),
+      PersistentBottomNavBarItem(
+        icon: Icon(Icons.settings),
+        title: ("Settings"),
+        activeColorPrimary: Colors.indigo,
+        inactiveColorPrimary: Colors
+            .grey, // routeAndNavigatorSettings: RouteAndNavigatorSettings(
+        //   initialRoute: '/',
+        //   routes: {
+        //     '/first': (context) => MainScreen2(),
+        //     '/second': (context) => MainScreen3(),
+        //   },
+        // ),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PersistentTabView(
+        context,
+        controller: _controller,
+        screens: _buildScreens(),
+        items: _navBarsItems(),
+        confineInSafeArea: true,
+        backgroundColor: Colors.white,
+        handleAndroidBackButtonPress: true,
+        resizeToAvoidBottomInset: true,
+        stateManagement: true,
+        navBarHeight: MediaQuery.of(context).viewInsets.bottom > 0
+            ? 0.0
+            : kBottomNavigationBarHeight,
+        hideNavigationBarWhenKeyboardShows: true,
+        margin: EdgeInsets.all(0.0),
+        popActionScreens: PopActionScreensType.all,
+        bottomScreenMargin: 0.0,
+        // onWillPop: (context) async {
+        //
+        //   return false;
+        // },
+        selectedTabScreenContext: (context) {
+          var testContext = context;
+        },
+        hideNavigationBar: _hideNavBar,
+        decoration: NavBarDecoration(
+            colorBehindNavBar: Colors.indigo,
+            borderRadius: BorderRadius.circular(20.0)),
+        popAllScreensOnTapOfSelectedTab: true,
+        itemAnimationProperties: ItemAnimationProperties(
+          duration: Duration(milliseconds: 400),
+          curve: Curves.ease,
+        ),
+        screenTransitionAnimation: ScreenTransitionAnimation(
+          animateTabTransition: true,
+          curve: Curves.ease,
+          duration: Duration(milliseconds: 200),
+        ),
+        navBarStyle:
+            NavBarStyle.style1, // Choose the nav bar style with this property
+      ),
+    );
+  }
+}
+
+// ----------------------------------------- Custom Style ----------------------------------------- //
+
+class CustomNavBarWidget extends StatelessWidget {
+  final int selectedIndex;
+  final List<PersistentBottomNavBarItem> items;
+  final ValueChanged<int> onItemSelected;
+
+  CustomNavBarWidget({
+    Key? key,
+    this.selectedIndex = 0,
+    required this.items,
+    required this.onItemSelected,
+  });
+
+  Widget _buildItem(PersistentBottomNavBarItem item, bool isSelected) {
+    return Container(
+      alignment: Alignment.center,
+      height: kBottomNavigationBarHeight,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Flexible(
+            child: IconTheme(
+              data: IconThemeData(
+                  size: 26.0,
+                  color: isSelected
+                      ? (item.activeColorSecondary == null
+                          ? item.activeColorPrimary
+                          : item.activeColorSecondary)
+                      : item.inactiveColorPrimary == null
+                          ? item.activeColorPrimary
+                          : item.inactiveColorPrimary),
+              child: isSelected ? item.icon : item.inactiveIcon ?? item.icon,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 5.0),
+            child: Material(
+              type: MaterialType.transparency,
+              child: FittedBox(
+                  child: Text(
+                item.title.toString(),
+                style: TextStyle(
+                    color: isSelected
+                        ? (item.activeColorSecondary == null
+                            ? item.activeColorPrimary
+                            : item.activeColorSecondary)
+                        : item.inactiveColorPrimary,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 12.0),
+              )),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      child: Container(
+        width: double.infinity,
+        height: kBottomNavigationBarHeight,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: items.map((item) {
+            int index = items.indexOf(item);
+            return Flexible(
+              child: GestureDetector(
+                onTap: () {
+                  this.onItemSelected(index);
+                },
+                child: _buildItem(item, selectedIndex == index),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
