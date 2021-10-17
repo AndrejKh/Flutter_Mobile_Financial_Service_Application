2d719f87a9ca75a6ecbdaf74221cd54cab219a05
AndrejKh <silverwewel@gmail.com>
2021-10-17 17:38:00 +0600

    updates

--- lib/views/screens/profile_screen.dart
+++ lib/views/screens/profile_screen.dart
@@ -19,12 +19,62 @@ class _ProfileScreenState extends State<ProfileScreen> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
         SizedBox(
-        height: 20,
+        height: 200,
+
       ),
-          
+          ListView(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            children: const <Widget>[
+              Card(child: ListTile(title: Text('One-line ListTile'))),
+              Card(
+                child: ListTile(
+                  leading: FlutterLogo(),
+                  title: Text('One-line with leading widget'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  title: Text('One-line with trailing widget'),
+                  trailing: Icon(Icons.more_vert),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: FlutterLogo(),
+                  title: Text('One-line with both widgets'),
+                  trailing: Icon(Icons.more_vert),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  title: Text('One-line dense ListTile'),
+                  dense: true,
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: FlutterLogo(size: 56.0),
+                  title: Text('Two-line ListTile'),
+                  subtitle: Text('Here is a second line'),
+                  trailing: Icon(Icons.more_vert),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: FlutterLogo(size: 72.0),
+                  title: Text('Three-line ListTile'),
+                  subtitle: Text(
+                      'A sufficiently long subtitle warrants three lines.'
+                  ),
+                  trailing: Icon(Icons.more_vert),
+                  isThreeLine: true,
+                ),
+              ),
+            ],
+          )
       ],
     ),
-
 
     );
   }
