edcbfdb6cd944e096748579da61e4f684db1bbee
AndrejKh <silverwewel@gmail.com>
2021-10-22 02:57:01 +0600

    Transaction history layout fix

--- lib/views/screens/transaction_history.dart
+++ lib/views/screens/transaction_history.dart
@@ -22,7 +22,7 @@ class _TransactionHistoryState extends State<TransactionHistory> {
             padding: const EdgeInsets.only(left: 65),
             child: Text(
             "Transaction History",
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500, ),
               ),
             ),
             SizedBox(
@@ -36,23 +36,37 @@ class _TransactionHistoryState extends State<TransactionHistory> {
                   borderRadius: BorderRadius.circular(10),
                 ),
                 child: Container(
+                  padding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
                   width: 400,
                   height: 70,
                   decoration: BoxDecoration(
-                     //border: Border.all(
+                     border: Border.all(
                        color: Colors.black12,
-                    //   width: 1,
-                    // ),
+                       width: 1,
+                     ),
                     borderRadius: BorderRadius.circular(10),
                   ),
                   child: Center(
-                      //child: Text("Name : ${widget.receiver}",
-                          //style: TextStyle(fontSize: 20))),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 30,
+                            backgroundImage: NetworkImage(
+                                'https://www.pngitem.com/pimgs/m/421-4213036_avatar-hd-png-download.png'),
+                          ),
+
+                          Text(" Name ", style: TextStyle(fontSize: 20)),
+                          Spacer(flex: 2,),
+                          Text("Amount", style: TextStyle(fontSize: 20),)
+                        ],
+                      ),
+                  ),
+
+
                 ),
               ),
-            ),
-
-            ),],
+            )
+          ],
         ),
 
     );
