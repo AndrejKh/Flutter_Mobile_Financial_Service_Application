405adf468ac81df9ba1e97e85fb918e66402c025
AndrejKh <silverwewel@gmail.com>
2021-10-22 02:07:51 +0600

    Update transaction_history.dart

--- lib/views/screens/transaction_history.dart
+++ lib/views/screens/transaction_history.dart
@@ -19,14 +19,40 @@ class _TransactionHistoryState extends State<TransactionHistory> {
           height: 10,
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 50, right: 50, bottom: 0),
+            padding: const EdgeInsets.only(left: 65),
             child: Text(
             "Transaction History",
             style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
               ),
-            )
+            ),
+            SizedBox(
+              height: 10,
+            ),
 
-         ],
+            Center(
+              child: Card(
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Container(
+                  width: 400,
+                  height: 70,
+                  decoration: BoxDecoration(
+                    // border: Border.all(
+                    //   color: Colors.black,
+                    //   width: 1,
+                    // ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                      //child: Text("Name : ${widget.receiver}",
+                          //style: TextStyle(fontSize: 20))),
+                ),
+              ),
+            ),
+
+            ),],
         ),
 
     );
