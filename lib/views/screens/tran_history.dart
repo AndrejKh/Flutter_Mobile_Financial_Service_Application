94cbc013680b3946f644cfcfc667ae4111ac8c58
AndrejKh <silverwewel@gmail.com>
2021-10-22 01:16:05 +0600

    Create tran_history.dart

---
+++ lib/views/screens/tran_history.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TransactionHistory extends StatefulWidget {
+  const TransactionHistory({Key? key}) : super(key: key);
+
+  @override
+  _TransactionHistoryState createState() => _TransactionHistoryState();
+}
+
+class _TransactionHistoryState extends State<TransactionHistory> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
