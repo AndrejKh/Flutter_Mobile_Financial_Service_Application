c4b2bc47316f79a89b809baac98007a63b68e80b
AndrejKh <silverwewel@gmail.com>
2021-10-21 18:03:32 +0600

    updated

--- lib/views/components/reuseable_widgets.dart
+++ lib/views/components/reuseable_widgets.dart
@@ -14,152 +14,211 @@ class DashBoardMainItemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          alignment: Alignment.centerRight,
-          height: 120,
-          child: PieChart(
-            PieChartData(
-              pieTouchData: PieTouchData(
-                  touchCallback: (FlTouchEvent event, pieTouchResponse) {
-                    setState(() {
-                      if (!event.isInterestedForInteractions ||
-                          pieTouchResponse == null ||
-                          pieTouchResponse.touchedSection == null) {
-                        touchedIndex = -1;
-                        return;
-                      }
-                      touchedIndex =
-                          pieTouchResponse.touchedSection!.touchedSectionIndex;
-                    });
-                  }),
-              borderData: FlBorderData(
-                show: false,
+    return Container(
+      child: Column(
+        children: [
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+              Card(
+                color: Colors.transparent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                elevation: 5,
+                child: SvgPicture.asset(asset),
               ),
-              sectionsSpace: 0,
-              centerSpaceRadius: 30,
-              sections: showingSections(),
-            ),
+              Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                      customBorder: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      onTap: () {
+                        onTap.call();
+                      },
+                      child: Container(
+                        height: 60,
+                        width: 60,
+                      )))
+            ],
           ),
-        ),
-      ],
-    );
-  }
-
-  List<PieChartSectionData> showingSections() {
-    return List.generate(3, (i) {
-      final isTouched = i == touchedIndex;
-      final fontSize = isTouched ? 25.0 : 16.0;
-      final radius = isTouched ? 30.0 : 15.0;
-      switch (i) {
-        case 0:
-          return PieChartSectionData(
-            showTitle: isTouched,
-            color: const Color(0xff0293ee),
-            value: 40,
-            title: '40%',
-            radius: radius,
-            titleStyle: TextStyle(
-                fontSize: fontSize,
-                fontWeight: FontWeight.bold,
-                color: const Color(0xffffffff)),
-          );
-        case 1:
-          return PieChartSectionData(
-            showTitle: isTouched,
-            color: const Color(0xfff8b250),
-            value: 30,
-            title: '30%',
-            radius: radius,
-            titleStyle: TextStyle(
-                fontSize: fontSize,
-                fontWeight: FontWeight.bold,
-                color: const Color(0xffffffff)),
-          );
-        case 2:
-          return PieChartSectionData(
-            showTitle: isTouched,
-            color: const Color(0xff13d38e),
-            value: 15,
-            title: '15%',
-            radius: radius,
-            titleStyle: TextStyle(
-                fontSize: fontSize,
-                fontWeight: FontWeight.bold,
-                color: const Color(0xffffffff)),
-          );
-        default:
-          throw Error();
-      }
-    });
-  }
-}
-
-class Indicator extends StatelessWidget {
-  final Color color;
-  final String text;
-  final double size;
-  final Color textColor;
-
-  const Indicator({
-    Key? key,
-    required this.color,
-    required this.text,
-    this.size = 16,
-    this.textColor = const Color(0xff505050),
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(5.0),
-      child: Card(
-        elevation: 3,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Container(
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: color,
-            ),
-            child: Text(
-              text,
+          SizedBox(height: 5),
+          Text(title,
               style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            )),
+                color: Colors.white,
+              ))
+        ],
       ),
     );
   }
 }
 
-class IndicatorRow extends StatelessWidget {
-  const IndicatorRow({Key? key}) : super(key: key);
+class PayBillsItem extends StatelessWidget {
+  final String asset;
+  final String title;
+  const PayBillsItem({Key? key, required this.asset, required this.title})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom: 10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Indicator(
-            color: Color(0xff0293ee),
-            text: '40% Send Money',
+      padding: const EdgeInsets.all(5.0),
+      child: Container(
+        // height: 80,
+        // width: 55,
+          child: Column(
+            children: [
+              Card(
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Container(
+                  padding: const EdgeInsets.all(8.0),
+                  height: 55,
+                  width: 55,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Image.asset(asset),
+                ),
+              ),
+              SizedBox(height: 2),
+              Text(title)
+            ],
+          )),
+    );
+  }
+}
+
+class CustomBackButton extends StatelessWidget {
+  const CustomBackButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+        child: Row(
+          children: [
+            ClipOval(
+              child: Material(
+                elevation: 10,
+                color: Colors.grey
+                    .shade200, //To let the Container background be displayed
+                child: IconButton(
+                    icon: Icon(
+                      Icons.arrow_back_ios_outlined,
+                      color: primaryColor,
+                    ),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    }),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomScaffold extends StatelessWidget {
+  final Widget body;
+  final bool isRoot;
+  const CustomScaffold({Key? key, required this.body, this.isRoot = false})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double h = MediaQuery.of(context).size.height;
+    double w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+                height: h,
+                width: w,
+                child: Image.asset("assets/img/bck_r.png")),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Container(
+                  alignment: Alignment.topRight,
+                  width: double.infinity,
+                  height: 300,
+                  child: Stack(
+                    children: [
+                      Image.asset("assets/img/vector_1_r.png"),
+                      Image.asset("assets/img/vector_2_r.png"),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (!isRoot) CustomBackButton(),
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 20, 8, 10),
+                    child: SingleChildScrollView(
+                      child: body,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomPrimaryButton extends StatelessWidget {
+  final String btnText;
+  // final Color btnColor;
+  final Function onTap;
+  const CustomPrimaryButton(
+      {Key? key, required this.btnText, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: Container(
+          alignment: Alignment.center,
+          height: 50,
+          width: 250,
+          child: Text(btnText,
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w500,
+              )),
+        ),
+        style: ElevatedButton.styleFrom(
+          elevation: 10,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
           ),
-          Indicator(
-            color: const Color(0xfff8b250),
-            text: '20% Recharge',
-          ),
-          Indicator(
-            color: const Color(0xff13d38e),
-            text: '40% Cashout',
-          ),
-        ],
+          primary: primaryColor,
+        ),
+        onPressed: () {
+          onTap.call();
+          // Navigator.push(
+          //     context,
+          //     MaterialPageRoute(
+          //         builder: (context) =>
+          //             SendMoneyConfirmation()));
+        },
       ),
     );
   }
