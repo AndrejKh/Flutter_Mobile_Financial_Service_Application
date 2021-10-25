38fbc75a4c9c32de03a36fe4a1bf55470157a7e8
AndrejKh <silverwewel@gmail.com>
2021-10-26 04:03:03 +0600

    Create notification_screen.dart

---
+++ lib/views/screens/notification_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Notification extends StatefulWidget {
+  const Notification({Key? key}) : super(key: key);
+
+  @override
+  _NotificationState createState() => _NotificationState();
+}
+
+class _NotificationState extends State<Notification> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
