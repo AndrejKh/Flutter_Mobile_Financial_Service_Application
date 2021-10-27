4f8e1a13ac2a68d411d1608beac3736d0d02715b
AndrejKh <silverwewel@gmail.com>
2021-10-27 13:42:15 +0600

    Create card notification

--- lib/views/screens/notification_screen.dart
+++ lib/views/screens/notification_screen.dart
@@ -16,15 +16,61 @@ class _NotificationScreenState extends State<NotificationScreen> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
         SizedBox(
-        height: 10,
-      ),
-      Padding(
-        padding: const EdgeInsets.only(left: 100, ),
-        child: Text(
-          "Notifications",
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500, ),
+          height: 10,
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.only(left: 100, ),
+          child: Text(
+            "Notifications",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500, ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+          
+          Center(
+            child: Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
+                width: 400,
+                height: 70,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black12,
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            'https://www.pngitem.com/pimgs/m/421-4213036_avatar-hd-png-download.png'),
+                      ),
+
+                      Column(
+                        children:[
+                          Text(" Name ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+
+                          Text("22-Oct-2021     12:19 AM",style: TextStyle(fontSize: 15, color: Colors.grey),),
+                        ],
+                      ),
+                      Spacer(flex: 2,),
+                      Text("+ 12,000 Tk", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.green),)
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+
       ]
       )
     );
