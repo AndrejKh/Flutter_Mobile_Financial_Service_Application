64ea8f48d4fce684f133c5313b5cab7305f9b53d
AndrejKh <silverwewel@gmail.com>
2021-10-28 01:43:45 +0600

    New card create

--- lib/views/screens/notification_screen.dart
+++ lib/views/screens/notification_screen.dart
@@ -76,6 +76,148 @@ class _NotificationScreenState extends State<NotificationScreen> {
             ),
           ),
 
+
+          Center(
+            child: Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
+                width: 400,
+                height: 70,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black12,
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            'https://www.pngitem.com/pimgs/m/421-4213036_avatar-hd-png-download.png'),
+                      ),
+
+                      Column(
+                        children:[
+                          Text(" New Offer on Payment ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                          Text("Offer Details .....",style: TextStyle(fontSize: 15, color: Colors.grey))
+
+                        ],
+                      ),
+                      //Spacer(flex: 2,),
+                      Column(
+                          children:[
+                            Text("12:19 AM", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey),),
+                            Text("22-Oct-2021",style: TextStyle(fontSize: 15, color: Colors.grey),)
+                          ]
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+
+          Center(
+            child: Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
+                width: 400,
+                height: 70,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black12,
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            'https://www.pngitem.com/pimgs/m/421-4213036_avatar-hd-png-download.png'),
+                      ),
+
+                      Column(
+                        children:[
+                          Text(" New Offer on Payment ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                          Text("Offer Details .....",style: TextStyle(fontSize: 15, color: Colors.grey))
+
+                        ],
+                      ),
+                      //Spacer(flex: 2,),
+                      Column(
+                          children:[
+                            Text("12:19 AM", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey),),
+                            Text("22-Oct-2021",style: TextStyle(fontSize: 15, color: Colors.grey),)
+                          ]
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          Center(
+            child: Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
+                width: 400,
+                height: 70,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black12,
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            'https://www.pngitem.com/pimgs/m/421-4213036_avatar-hd-png-download.png'),
+                      ),
+
+                      Column(
+                        children:[
+                          Text(" New Offer on Payment ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                          Text("Offer Details .....",style: TextStyle(fontSize: 15, color: Colors.grey))
+
+                        ],
+                      ),
+                      //Spacer(flex: 2,),
+                      Column(
+                          children:[
+                            Text("12:19 AM", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey),),
+                            Text("22-Oct-2021",style: TextStyle(fontSize: 15, color: Colors.grey),)
+                          ]
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
       ]
       )
     );
