c104a5aa5d25aa529f5c0b57757d44acd694610a
AndrejKh <silverwewel@gmail.com>
2021-11-02 01:17:19 +0600

    Revert "notification edited"
    This reverts commit dbef02a464486e8de02a18608ec922eba2c9b0cc.

--- lib/views/screens/notification_screen.dart
+++ lib/views/screens/notification_screen.dart
@@ -1,180 +1,273 @@
 import 'package:etaka/views/components/reuseable_widgets.dart';
 import 'package:flutter/material.dart';
 
-class OTPScreen extends StatefulWidget {
-  final String phone_number;
-  const OTPScreen({Key? key, required this.phone_number}) : super(key: key);
+class NotificationScreen extends StatefulWidget {
+  const NotificationScreen({Key? key}) : super(key: key);
+
   @override
-  _OTPScreenState createState() => _OTPScreenState();
+  _NotificationScreenState createState() => _NotificationScreenState();
 }
 
-class _OTPScreenState extends State<OTPScreen> {
-  String text = '';
-
-  void _onKeyboardTap(String value) {
-    setState(() {
-      text = text + value;
-    });
-  }
-
-  Widget otpNumberWidget(int position) {
-    try {
-      return Container(
-        height: 40,
-        width: 40,
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.black, width: 0),
-            borderRadius: const BorderRadius.all(Radius.circular(8))),
-        child: Center(
-            child: Text(
-              text[position],
-              style: TextStyle(color: Colors.black),
-            )),
-      );
-    } catch (e) {
-      return Container(
-        height: 40,
-        width: 40,
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.black, width: 0),
-            borderRadius: const BorderRadius.all(Radius.circular(8))),
-      );
-    }
-  }
-
+class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
-      // backgroundColor: Colors.white,
-      // appBar: AppBar(
-      //   leading: IconButton(
-      //     icon: Container(
-      //       padding: const EdgeInsets.all(10),
-      //       decoration: BoxDecoration(
-      //         borderRadius: const BorderRadius.all(Radius.circular(20)),
-      //         color: primaryColor.withAlpha(20),
-      //       ),
-      //       child: Icon(
-      //         Icons.arrow_back_ios,
-      //         color: primaryColor,
-      //         size: 16,
-      //       ),
-      //     ),
-      //     onPressed: () => Navigator.of(context).pop(),
-      //   ),
-      //   elevation: 0,
-      //   backgroundColor: Colors.white,
-      //   brightness: Brightness.light,
-      // ),
       body: Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          Container(
-            height: 300,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Text(
-                        'Enter 6 Digits Verification Code Sent To Your Number',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 26,
-                            fontWeight: FontWeight.w500))),
-                Container(
-                  constraints: const BoxConstraints(maxWidth: 500),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        SizedBox(
+          height: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 100, ),
+          child: Text(
+            "Notifications",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500, ),
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+          
+          Center(
+            child: Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
+                width: 400,
+                height: 70,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black12,
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      otpNumberWidget(0),
-                      otpNumberWidget(1),
-                      otpNumberWidget(2),
-                      otpNumberWidget(3),
-                      otpNumberWidget(4),
-                      otpNumberWidget(5),
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            'https://www.pngitem.com/pimgs/m/421-4213036_avatar-hd-png-download.png'),
+                      ),
+
+                      Column(
+                        children:[
+                          Text(" New Offer on Payment ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                          Text("Offer Details .....",style: TextStyle(fontSize: 15, color: Colors.grey))
+
+                        ],
+                      ),
+                      //Spacer(flex: 2,),
+                      Column(
+                        children:[
+                          Text("12:19 AM", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey),),
+                          Text("22-Oct-2021",style: TextStyle(fontSize: 15, color: Colors.grey),)
+                        ]
+                      ),
                     ],
                   ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            constraints: const BoxConstraints(maxWidth: 500),
-            child: RaisedButton(
-              onPressed: () async {
-                print(text);
-                APIService api = APIService();
-                bool correct = await api.otpcheck(widget.phone_number, text);
-                if (correct) {
-                  bool ch = await api.checkAccountExist();
-                  if (ch) {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => LoginScreen()),
-                    );
-                  } else {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => RegistrationScreen(),
-                      ),
-                    );
-                  }
-                } else {
-                  error_toast("Invalid OTP");
-                }
-              },
-              color: primaryColor,
-              shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(14))),
-              child: Container(
-                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      'Confirm',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.all(8),
-                      decoration: BoxDecoration(
-                        borderRadius:
-                        const BorderRadius.all(Radius.circular(20)),
-                        color: primaryColor,
-                      ),
-                      child: Icon(
-                        Icons.arrow_forward_ios,
-                        color: Colors.white,
-                        size: 16,
-                      ),
-                    )
-                  ],
                 ),
               ),
             ),
           ),
-          NumericKeyboard(
-            onKeyboardTap: _onKeyboardTap,
-            textColor: primaryColor,
-            rightIcon: Icon(
-              Icons.backspace,
-              color: primaryColor,
+
+
+          Center(
+            child: Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
+                width: 400,
+                height: 70,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black12,
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            'https://www.pngitem.com/pimgs/m/421-4213036_avatar-hd-png-download.png'),
+                      ),
+
+                      Column(
+                        children:[
+                          Text(" New Offer on Payment ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                          Text("Offer Details .....",style: TextStyle(fontSize: 15, color: Colors.grey))
+
+                        ],
+                      ),
+                      //Spacer(flex: 2,),
+                      Column(
+                          children:[
+                            Text("12:19 AM", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey),),
+                            Text("22-Oct-2021",style: TextStyle(fontSize: 15, color: Colors.grey),)
+                          ]
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-            rightButtonFn: () {
-              setState(() {
-                if (text.length > 0) {
-                  text = text.substring(0, text.length - 1);
-                }
-              });
-            },
-          )
-        ],
-      ),
+          ),
+
+
+          Center(
+            child: Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
+                width: 400,
+                height: 70,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black12,
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            'https://www.pngitem.com/pimgs/m/421-4213036_avatar-hd-png-download.png'),
+                      ),
+
+                      Column(
+                        children:[
+                          Text(" New Offer on Payment ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                          Text("Offer Details .....",style: TextStyle(fontSize: 15, color: Colors.grey))
+
+                        ],
+                      ),
+                      //Spacer(flex: 2,),
+                      Column(
+                          children:[
+                            Text("12:19 AM", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey),),
+                            Text("22-Oct-2021",style: TextStyle(fontSize: 15, color: Colors.grey),)
+                          ]
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          Center(
+            child: Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
+                width: 400,
+                height: 70,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black12,
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            'https://www.pngitem.com/pimgs/m/421-4213036_avatar-hd-png-download.png'),
+                      ),
+
+                      Column(
+                        children:[
+                          Text(" New Offer on Payment ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                          Text("Offer Details .....",style: TextStyle(fontSize: 15, color: Colors.grey))
+
+                        ],
+                      ),
+                      //Spacer(flex: 2,),
+                      Column(
+                          children:[
+                            Text("12:19 AM", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey),),
+                            Text("22-Oct-2021",style: TextStyle(fontSize: 15, color: Colors.grey),)
+                          ]
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+
+          Center(
+            child: Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Container(
+                padding: const EdgeInsets.fromLTRB(4, 4, 4, 4),
+                width: 400,
+                height: 70,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black12,
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            'https://www.pngitem.com/pimgs/m/421-4213036_avatar-hd-png-download.png'),
+                      ),
+
+                      Column(
+                        children:[
+                          Text(" New Offer on Payment ", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+                          Text("Offer Details .....",style: TextStyle(fontSize: 18, color: Colors.grey))
+
+                        ],
+                      ),
+                      //Spacer(flex: 2,),
+                      Column(
+                          children:[
+                            Text("12:19 AM", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.grey),),
+                            Text("22-Oct-2021",style: TextStyle(fontSize: 15, color: Colors.grey),)
+                          ]
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+      ]
+      )
     );
   }
 }
